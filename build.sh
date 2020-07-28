3d88d3921fa92ab629357dc655c2a0d9bea2019e
Blair <blair@blairjames.com>
2020-07-28 22:56:23 +1000

    Automatic build 20200728_2256

--- build.sh
+++ build.sh
@@ -3,24 +3,32 @@
 # Env Vars for SSH
 source /root/.ssh/agent/root || . /root/.ssh/agent/root
 
-# Print
-print () {
-    printf "$1\n"
-}
+# Log file
+log="/var/log/messages"
 
 # Build current timestamp
 timestamp () {
   date +"%Y%m%d_%H%M"
 }
 
-# Static start time.
-ts=$(timestamp)
-
 # Exception Catcher
 except () {
     print $1
     return 1
 }
+
+# Print
+printer () {
+    printf "$1\n" || except "Printf failed in the print function!"
+    echo "$1" >> $log || except "Echo failed in the print function!"
+}
+if ! printer; then
+    except "Print Failed!"
+    exit 1
+fi
+
+# Static start time.
+ts=$(timestamp)
 
 # Run the build and push to Git and Docker.
 run () {
