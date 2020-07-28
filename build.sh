b157348ae9deafeee9072c8ab1a7af338de592d0
Blair <blair@blairjames.com>
2020-07-28 22:47:14 +1000

    Automatic build 20200728_2247

--- build.sh
+++ build.sh
@@ -1,17 +1,43 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-timestamp() {
+# Env Vars for SSH
+source /root/.ssh/agent/root || . /root/.ssh/agent/root
+
+# Print
+print () {
+    printf "$1\n"
+}
+
+# Build current timestamp
+timestamp () {
   date +"%Y%m%d_%H%M"
 }
 
+# Static start time.
 ts=$(timestamp)
-echo "$ts"
 
-. /root/.ssh/agent/root 
+# Exception Catcher
+except () {
+    print $1
+    return 1
+}
 
-docker build . -t blairy/sslscan:$ts
-git pull && \
-git add --all && \ 
-git commit -a -m "Automatic build $ts" && \ 
-git push
-docker push blairy/sslscan:$ts
+# Run the build and push to Git and Docker.
+run () {
+    docker build /home/docker/sslscan/sslscan_docker_image/Dockerfile -t blairy/sslscan:$ts || except "Docker build failed!" 
+    git="/usr/bin/git -C /home/docker/sslscan/sslscan_docker_image/"
+    $git pull && \
+    $git add --all && \
+    $git commit -a -m "Automatic build $ts" && \ 
+    $git push || except "Git Failed!"
+    docker push blairy/sslscan:$ts || except "Docker push failed!"
+}
+
+# Manage execution
+if run; then
+    print "Build and Push successfull!"
+else
+    except "Run Failed!"
+    exit 1
+fi
+exit 0
